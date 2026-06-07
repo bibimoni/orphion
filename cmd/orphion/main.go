0690efa3da81efcd40f880266bb4b0252517f8f3
bibimoni <proquadi66@gmail.com>
2026-06-07 14:03:29 +0700

    feat: add signal handling and final CLI wiring

--- cmd/orphion/main.go
+++ cmd/orphion/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/distiled/orphion/internal/cli"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt, syscall.SIGTERM,
+	)
+	defer cancel()
+
+	_ = ctx
+
 	if err := cli.New(nil).Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, "orphion:", err)
-		os.Exit(1)
+		os.Exit(exitCodeFor(err))
 	}
+}
+
+func exitCodeFor(err error) int {
+	if err == nil {
+		return 0
+	}
+	return 1
 }
